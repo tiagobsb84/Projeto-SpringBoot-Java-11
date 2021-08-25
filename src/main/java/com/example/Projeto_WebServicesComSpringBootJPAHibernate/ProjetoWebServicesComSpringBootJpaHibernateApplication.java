8036312a21342c28ca527d05942db4e35a966429
unknown <tiagobsb84@gmail.com>
2021-08-25 09:36:50 -0300

    Criando projeto

---
+++ src/main/java/com/example/Projeto_WebServicesComSpringBootJPAHibernate/ProjetoWebServicesComSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_WebServicesComSpringBootJPAHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesComSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projeto_WebServicesComSpringBootJPAHibernate/ProjetoWebServicesComSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_WebServicesComSpringBootJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
